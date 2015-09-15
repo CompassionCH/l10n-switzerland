3863754325d738e605d5f756ab73fd34a70b88ec
Emanuel Cino <ecino@compassion.ch>
2015-09-15 11:25:07 +0200

    Code corrections

--- l10n_ch_fds_postfinance/model/fds_postfinance_files.py
+++ l10n_ch_fds_postfinance/model/fds_postfinance_files.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Swiss Postfinance File Delivery Services module for Odoo
-#    Copyright (C) 2014 Compassion CH
+#    Copyright (C) 2015 Compassion CH
 #    @author: Nicolas Tran
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -20,31 +20,30 @@
 #
 ##############################################################################
 
-from openerp import models, fields, api, exceptions
+from openerp import models, fields, api, exceptions, _
 import logging
 
 _logger = logging.getLogger(__name__)
 
 
 class fds_postfinance_files(models.Model):
-    ''' Model of the informations and files downloaded on FDS PostFinance
+    ''' Model of the information and files downloaded on FDS PostFinance
         (Keep files in the database)
     '''
     _name = 'fds.postfinance.files'
 
     fds_account_id = fields.Many2one(
         comodel_name='fds.postfinance.account',
-        string='FDS account id',
+        string='FDS account',
         ondelete='restrict',
         readonly=True,
-        help='file related to FDS account id'
+        help='file related to FDS account'
     )
     files = fields.Binary(
-        string='Files',
         readonly=True,
         help='the downloaded file'
     )
-    bankStatment_id = fields.Many2one(
+    bank_statement_id = fields.Many2one(
         comodel_name='account.bank.statement',
         string='Bank Statment id',
         ondelete='restrict',
@@ -52,9 +51,7 @@ class fds_postfinance_files(models.Model):
         help='the generate bank statment id'
     )
     filename = fields.Char(
-        string='Filename',
-        readonly=True,
-        help='The name of the file'
+        readonly=True
     )
     directory_id = fields.Many2one(
         'fds.postfinance.files.directory',
@@ -73,7 +70,7 @@ class fds_postfinance_files(models.Model):
     )
     state = fields.Selection(
         selection=[('draft', 'Draft'),
-                   ('finish', 'Finish'),
+                   ('done', 'Done'),
                    ('error', 'Error')],
         readonly=True,
         default='draft',
@@ -93,7 +90,7 @@ class fds_postfinance_files(models.Model):
         self.ensure_one()
 
         if not self.directory_id.journal_id:
-            raise exceptions.Warning('Add default journal in acount conf')
+            raise exceptions.Warning(_('Add default journal in acount conf'))
         self.import2bankStatements()
 
     @api.multi
@@ -113,8 +110,7 @@ class fds_postfinance_files(models.Model):
 
             :return None:
         '''
-        self.ensure_one()
-        self.write({'state': 'draft'})
+        self.state = 'draft'
 
     ##############################
     #          function          #
@@ -136,15 +132,15 @@ class fds_postfinance_files(models.Model):
             bs_imoprt_obj = self.env['account.bank.statement.import']
             bank_wiz_imp = bs_imoprt_obj.create(values)
             bank_wiz_imp.import_file()
-            self._state_finish_on()
+            self._state_done_on()
             self._add_bankStatement_ref()
             self._remove_binary_file()
             _logger.info("[OK] import file '%s' to bank Statements",
                          (self.filename))
             return True
         except:
-            _logger.info("[FAIL] import file '%s' to bank Statements",
-                         (self.filename))
+            _logger.warning("[FAIL] import file '%s' to bank Statements",
+                            (self.filename))
             return False
 
     @api.multi
@@ -156,7 +152,7 @@ class fds_postfinance_files(models.Model):
         bs = self.env['account.bank.statement'].search([
             ['state', '=', 'draft'],
             ['create_uid', '=', self.env.uid]])
-        self.write({'bankStatment_id': max(bs).id})
+        self.write({'bank_statement_id': max(bs).id})
 
     @api.multi
     def _remove_binary_file(self):
@@ -168,13 +164,13 @@ class fds_postfinance_files(models.Model):
         self.write({'files': None})
 
     @api.multi
-    def _state_finish_on(self):
-        ''' private function that change state to finish
+    def _state_done_on(self):
+        ''' private function that change state to done
 
             :returns: None
         '''
         self.ensure_one()
-        self.write({'state': 'finish'})
+        self.write({'state': 'done'})
 
     def _sate_error_on(self):
         ''' private function that change state to error
