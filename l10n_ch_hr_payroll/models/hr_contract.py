0e15713d1020e3f7360fea6ca9683c8737d638a4
ecino <ecino@users.noreply.github.com>
2019-01-17 15:54:10 +0100

    Update l10n_ch_hr_payroll/models/hr_contract.py
    use code account instead of id
    
    Co-Authored-By: davidwul <david@compassion.ch>

--- l10n_ch_hr_payroll/models/hr_contract.py
+++ l10n_ch_hr_payroll/models/hr_contract.py
@@ -52,6 +52,7 @@ class HrContract(models.Model):
         for contract in self:
             move_lines = self.env['account.move.line'].search([
                 ('partner_id', '=', contract.employee_id.address_home_id.id),
-                ('account_id', '=', 172)
+                ('account_id', '=', self.env['account.account'].search(
+                    ['code', '=', '2291']).id)
             ])
             contract.provision_13_salary = sum(move_lines.mapped('credit')) - sum(move_lines.mapped('debit'))
