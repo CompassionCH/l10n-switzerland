e807b94982b23e4024a4fb16ca3416393ea87e82
Theyiot <pprizee@gmail.com>
2019-06-11 12:30:07 +0200

    Changes revert, and style fix in another file

--- l10n_ch_bank_statement_import_postfinance/models/postfinance_file_parser.py
+++ l10n_ch_bank_statement_import_postfinance/models/postfinance_file_parser.py
@@ -21,20 +21,27 @@ from os.path import splitext
 from tarfile import TarFile, TarError
 from cStringIO import StringIO
 from lxml import etree
-from io import BytesIO
+#from io import BytesIO
 
 from odoo import models
 
 _logger = logging.getLogger(__name__)
 
 
-try:
+'''try:
     from PIL import Image
-    wand = True
+    pil = True
 except ImportError:
     _logger.warning('Please install Pillow (sudo pip install Pillow) for '
                     'Postfinance bank statement import supporting tiff '
                     'attachments.')
+    pil = None'''
+try:
+    from wand.image import Image
+    wand = True
+except ImportError:
+    _logger.warning('Please install Wand (sudo pip wand) for Postfinance '
+                    'bank statement import supporting tiff attachments.')
     wand = None
 
 
@@ -130,7 +137,7 @@ class XMLPFParser(models.AbstractModel):
             attachments = {}
             tar_file = TarFile.open(fileobj=pf_file, mode="r:gz")
             accepted_formats = ['.png', '.jpeg', '.jpg']
-            if wand:
+            if pil:
                 accepted_formats.append('.tiff')
             for file_name in tar_file.getnames():
                 accepted = reduce(lambda x, y: x or y, [
@@ -141,12 +148,15 @@ class XMLPFParser(models.AbstractModel):
                     img_data = tar_file.extractfile(file_name).read()
                     if file_name.endswith('.tiff'):
                         # Convert string containing data to tiff image
-                        image = Image.open(BytesIO(img_data))
+                        '''image = Image.open(BytesIO(img_data))
 
                         # Convert to png for viewing the image in Odoo
                         with BytesIO() as png_image:
                             image.save(png_image, format='PNG')
-                            img_data = png_image.getvalue()
+                            img_data = png_image.getvalue()'''
+                        with Image(blob=img_data) as img:
+                            img.format = 'png'
+                            img_data = img.make_blob()
                     attachments[key] = img_data.encode('base64')
             return attachments
         except TarError:
