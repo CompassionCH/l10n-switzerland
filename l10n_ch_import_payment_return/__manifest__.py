6baa7a3a2be0ce6da4e04cac02a77d033156b3d3
Simon Gonzalez <sgonzalez@ikmail.com>
2023-06-23 12:42:06 +0200

    [ADD] l10n_ch_import_payment_return module to handle return from payment orders Add functionalities to handle the payment return from the switzerland pain generated from payment orders

---
+++ l10n_ch_import_payment_return/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2023 Compassion CH (<https://www.compassion.ch>)
+# @author: Simon Gonzalez <simon.gonzalez@bluewin.ch>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': "l10n_ch_import_payment_return",
+    "version": "14.0.1.0.0",
+    "development_status": "Beta",
+    "license": "AGPL-3",
+    "author": "Compassion CH,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/bank-payment",
+    "category": "Banking addons",
+    "depends": [
+        "account_payment_export_sftp"  # OCA/bank-payment
+    ],
+    "data": [
+        "data/edi_data.xml"
+    ],
+    "installable": True,
+}
