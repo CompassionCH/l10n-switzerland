768f3dc0dc7bbf84302d78fc18361c3d7ad8991b
Simon Gonzalez <sgonzalez@ikmail.com>
2023-06-30 09:08:56 +0200

    T0350 : Constraints

--- l10n_ch_bank_statement_import_camt/models/account_journal.py
+++ l10n_ch_bank_statement_import_camt/models/account_journal.py
@@ -1,7 +1,7 @@
-# Copyright 2019 ACSONE SA/NV <thomas.binsfeld@acsone.eu>
+# Copyright 2023 Compassion CH <https://compassion.ch>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models, _
+from odoo import fields, models, _, api
 
 
 class AccountJournal(models.Model):
@@ -12,3 +12,16 @@ class AccountJournal(models.Model):
         help="Parse the account QR iban field for CAMT54\n"
              "This field can't accept three journals with the same account number."
     )
+
+    @api.model
+    def _qr_iban_defined_qr_parsing(self):
+        for journal in self:
+            if journal.should_qr_parsing and not journal.bank_account_id.l10n_ch_qr_iban:
+                return False
+        return True
+
+    _constraints = [(
+        _qr_iban_defined_qr_parsing,
+        'The QR IBAN should be defined on the bank account if you use QR IBAN for import on the journal',
+        ['should_qr_parsing']
+    )]
