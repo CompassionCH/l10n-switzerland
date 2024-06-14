f19959d667769889ab68f68a803d2fdfa62382cd
David Wulliamoz <dwulliamoz@compassion.ch>
2024-06-14 10:53:25 +0200

    condition to compute postal ref only if account is a postal type AND include related info

--- l10n_ch_pain_direct_debit/models/account_payment_order.py
+++ l10n_ch_pain_direct_debit/models/account_payment_order.py
@@ -205,9 +205,9 @@ class AccountPaymentOrder(models.Model):
             party_agent_clearing_identification.text = (
                 partner_bank.acc_number.replace(" ", "")[4:9]
             )
-            ccp_other = etree.SubElement(party_agent_institution, "Othr")
-            ccp_other_id = etree.SubElement(ccp_other, "Id")
             if self.company_partner_bank_id.acc_type == 'postal':
+                ccp_other = etree.SubElement(party_agent_institution, "Othr")
+                ccp_other_id = etree.SubElement(ccp_other, "Id")
                 ref_subparts = self.company_partner_bank_id.l10n_ch_postal.split("-")
                 ccp_other_id.text = (
                     ref_subparts[0] + ref_subparts[1].rjust(6, "0") + ref_subparts[2]
