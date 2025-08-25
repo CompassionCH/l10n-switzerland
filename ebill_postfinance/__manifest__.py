4dc615a95cb855f5ea4aa2e4f667d0ee604021a7
davidwul <david@compassion.ch>
2025-08-25 17:37:01 +0200

    Update __manifest__.py

--- ebill_postfinance/__manifest__.py
+++ ebill_postfinance/__manifest__.py
@@ -13,7 +13,6 @@
         "account",
         "account_invoice_export",
         "base_ebill_payment_contract",
-        "l10n_ch_base_bank",
         "l10n_ch_qriban",
         "queue_job",
         "sale",
