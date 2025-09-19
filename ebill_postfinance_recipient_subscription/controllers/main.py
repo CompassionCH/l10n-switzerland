cd67bd3830b6df11a2e31f08540b035673da8a2a
EliasKeller <elias.keller@gmail.com>
2025-09-19 17:05:03 +0200

    [T2614] FEAT: extend controller with init /ebill/subscribe route

--- ebill_postfinance_recipient_subscription/controllers/main.py
+++ ebill_postfinance_recipient_subscription/controllers/main.py
@@ -1,6 +1,11 @@
 from odoo import http
 from odoo.http import request
 
+
 class EbillSubscriptionController(http.Controller):
-    # Hier kommen Ihre Web-Routen rein
-    pass
+
+    @http.route('/ebill/subscribe', type='http', auth='public', website=True)
+    def subscribe(self, **kw):
+        """Zeigt die erste Seite an, auf der der Benutzer seine E-Mail eingeben kann."""
+        return request.render('ebill_postfinance_recipient_subscription.subscribe_template', {})
+
