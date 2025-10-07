11da0f096edae0996f8cdfe909c11381d2871920
EliasKeller <elias.keller@gmail.com>
2025-10-07 16:40:43 +0200

    [T2614] FEAT: save contract into db correctly

--- ebill_postfinance_recipient_subscription/controllers/ebill_recipient_subscription_workflow.py
+++ ebill_postfinance_recipient_subscription/controllers/ebill_recipient_subscription_workflow.py
@@ -42,65 +42,71 @@ class EbillSubscriptionController(http.Controller):
     @http.route('/ebill/validate', type='http', auth='public', website=True, methods=['POST'])
     def validate(self, **post):
         email = post.get('email')
-        ebill_service = self._get_ebill_service()
+        try:
+            ebill_service = self._get_ebill_service()
+            token_sub = ebill_service.initiate_ebill_recipient_subscription(email)
 
-        token_sub = ebill_service.initiate_ebill_recipient_subscription(email)
-
-        return request.render('ebill_postfinance_recipient_subscription.validate_template', {
-            'token': token_sub.SubscriptionInitiationToken,
-            'email': email,
-        })
+            return request.render('ebill_postfinance_recipient_subscription.validate_template', {
+                'token': token_sub.SubscriptionInitiationToken,
+                'email': email,
+            })
+        except Exception as e:
+            _logger.error(f"Error during the eBill confirmation process for mail '{email}': {e}", exc_info=True)
+            return request.render('ebill_postfinance_recipient_subscription.retry_template')
 
     @http.route('/ebill/confirm', type='http', auth='public', website=True, methods=['POST'])
     def confirm(self, **post):
         token = post.get('token')
         activation_code = post.get('validation_code')
+        email = post.get('email')
+        ebill_service = self._get_ebill_service()
 
         try:
-            ebill_service = self._get_ebill_service()
             partner_data = ebill_service.confirm_ebill_recipient_subscription(
                 token, activation_code
             )
 
-            if not (partner_data and partner_data.get('EbillAccountID')):
-                _logger.warning(f"eBill validation failed for activation code '{activation_code}' (e.g., incorrect code).")
+            if not (partner_data and partner_data.get('eBillAccountID')):
+                _logger.warning(f"eBill validation failed for token '{token}' (e.g., incorrect code).")
                 return request.render('ebill_postfinance_recipient_subscription.subscribe_template',
                                       {'error': 'Validation failed. Please check the code.'})
 
-            if partner_data and partner_data.get('EbillAccountID'):
-                partner_email = partner_data.get('EmailAddress')
-                partner = request.env['res.partner'].sudo().search([('email', '=', partner_email)], limit=1)
-                if not partner:
+            partner_email = partner_data.get('eMailAddress')
+            partner = request.env['res.partner'].sudo().search([('email', '=', partner_email)], limit=1)
+            if not partner:
 
-                    partner_address = partner_data.get('Party', {}).get('Address', {})
+                partner_address = partner_data.get('Party', {}).get('Address', {})
 
-                    name = partner_address.get('GivenName') + partner_address.get('LastName') or partner_email.split('@')[0]
-                    street = partner_address.get('Address1')
-                    zip = partner_address.get('ZIP')
-                    city = partner_address.get('City')
+                name = partner_address.get('GivenName') + ' ' + partner_address.get('LastName') or partner_email.split('@')[0]
+                street = partner_address.get('Address1')
+                zip = partner_address.get('ZIP')
+                city = partner_address.get('City')
 
 
-                    partner = request.env['res.partner'].sudo().create({
-                        'name': name,
-                        'street': street,
-                        'zip': zip,
-                        'city': city,
-                        'email': partner_email
-                    })
+                partner = request.env['res.partner'].sudo().create({
+                    'name': name,
+                    'street': street,
+                    'zip': zip,
+                    'city': city,
+                    'email': partner_email
+                })
 
-                if partner:
-                    contract_vals = {
-                        'partner_id': partner.id,
-                        'postfinance_service_id': ebill_service.id,
-                        'ebill_account_id': partner_data.get('EbillAccountID'),
-                        'state': 'open',
-                    }
-                    request.env['ebill.payment.contract'].sudo().create(contract_vals)
-                    print("new contract gets created")
+            transmit_method_id = request.env['transmit.method'].sudo().search([('code', '=', 'postfinance')], limit=1)
+            request.env['ebill.payment.contract'].sudo().create({
+                'partner_id': partner.id,
+                'transmit_method_id': transmit_method_id.id,
+                'state': 'open',
+                'postfinance_service_id': ebill_service.id,
+                'postfinance_billerid': partner_data.get('eBillAccountID')
+            })
 
-                return request.render('ebill_postfinance_recipient_subscription.success_template', {})
+            return request.render('ebill_postfinance_recipient_subscription.success_template', {})
 
         except Exception as e:
+            _logger.warning(f"Exception during confirmation, likely a wrong activation code for token '{token}': {e}", exc_info=True)
 
-            _logger.error(f"Error during the eBill confirmation process for token '{token}' and activation code '{activation_code}'.", exc_info=True)
-            return request.render('ebill_postfinance_recipient_subscription.retry_template')
+            return request.render('ebill_postfinance_recipient_subscription.validate_template', {
+                'error': 'The Validation Code was wrong. Please try again or request a new one.',
+                'token': token,
+                'email': email
+            })
