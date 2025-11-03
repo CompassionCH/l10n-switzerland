b63c195bff15213f932ada27366c8dc71f317c8b
EliasKeller <38786773+EliasKeller@users.noreply.github.com>
2025-11-03 09:49:26 +0100

    Update ebill_postfinance_financial_discount/__manifest__.py
    Co-authored-by: ecino <ecino@users.noreply.github.com>

--- ebill_postfinance_financial_discount/__manifest__.py
+++ ebill_postfinance_financial_discount/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": "Integrate the discount from account_financial_discount into ebill_postfinance",
     "version": "14.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Compassion CH,Odoo Community Association (OCA)",
+    "author": "Camptocamp,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-switzerland",
     "depends": ["ebill_postfinance", "account_financial_discount"],
     "auto_install": True,
